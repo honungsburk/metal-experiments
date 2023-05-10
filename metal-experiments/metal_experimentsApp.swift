f6cede7fd7a3ac467965806dc80b90a84ad615fb
Frank Hampus Weslien <Frank.Weslien@geshdo.com>
2023-05-10 11:13:06 +0200

    Initial Commit

---
+++ metal-experiments/metal_experimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  metal_experimentsApp.swift
+//  metal-experiments
+//
+//  Created by Frank Hampus Weslien on 2023-05-10.
+//
+
+import SwiftUI
+
+@main
+struct metal_experimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
